a14e44734669a7d2972f2158faee7e3278c7edab
jinpill <jinlee@metamorp.cc>
2024-04-04 20:58:20 +0900

    useMaterialStore 추가

--- src/renderer/components/view/Models/index.tsx
+++ src/renderer/components/view/Models/index.tsx
@@ -1,8 +1,11 @@
+import { useMemo } from "react";
+import * as THREE from "three";
 import { STLLoader } from "three/examples/jsm/loaders/STLLoader";
 import { useLoader } from "@react-three/fiber";
 
 const Models = () => {
   const geometry = useLoader(STLLoader, "/sample-models/crown.stl");
+  const material = useMemo(() => {}, []);
 
   return (
     <group>
